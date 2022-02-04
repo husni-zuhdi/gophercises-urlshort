b575294c4c794895f0ed2afe3faf00ef2473a875
Husni Zuhdi <hazunanafaru@gmail.com>
2022-02-04 14:03:30 +0700

    Finish MapHandler

--- main/main.go
+++ main/main.go
@@ -21,16 +21,16 @@ func main() {
 	// fallback
 	yaml := `
 - path: /urlshort
-  url: https://github.com/gophercises/urlshort
+	url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
+	url: https://github.com/gophercises/urlshort/tree/solution
 `
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
