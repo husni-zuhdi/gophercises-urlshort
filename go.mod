a49608766806bc7a0435714529627abbef942495
Husni Zuhdi <hazunanafaru@gmail.com>
2022-02-04 14:26:41 +0700

    Add YAMLHandler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hazunanafaru/urlshort
 
 go 1.17
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -15,6 +17,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		path := r.URL.Path
 		if dest, ok := pathsToUrls[path]; ok {
 			http.Redirect(w, r, dest, http.StatusFound)
+			return
 		}
 		fallback.ServeHTTP(w, r)
 	}
@@ -37,6 +40,21 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
